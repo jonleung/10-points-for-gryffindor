3d32de0910b82654bc5ab186426104e9a3a3a96a
Jonathan Leung <me@jonl.org>
2015-01-16 16:34:15 -0500

    bad commit

--- js/wand/wand.js
+++ js/wand/wand.js
@@ -1,37 +1,37 @@
+var theFunctionToCallWhenTheDocumentIsReady = function() {
 
-$(document).ready(function() {
-
-  var soundFilenames = [
-    "Expeliarmus 1.mp3",
-    "Stupefix + Contre 1.mp3",
-    "Expeliarmus 2.mp3",
-    "Transplanage Arrive.mp3",
-    "Stupefix + Contre 2.mp3"
-  ];
-
-  var sounds = [];
-
-  $(soundFilenames).each(function(i, filename) {
-    sounds[i] = new Howl({urls: ['sounds/' + filename] });
-  });
-
-  $("body").on("click touchmove", function() {
-
-    sounds.forEach(function(sound) {
-      sound.play();
-    });
-
-
-
-  });
+  //============================================
+  // Shared Variables
 
   var THRESHOLD_MAGNITUDE = 30;
-
+  var DWEET_CHANNEL = "jonathan-leung-channel"
   var canTrigger = true;
+  var sounds = [];
+
+  //============================================
+  // Shared Variables
+
+  var dweet = function(channel) {
+    dweetio.dweet_for(DWEET_CHANNEL, {time: new Date(), type: "trigger"});
+  }
+
+  //============================================
+  // Trigger The Wand
+
+  var waveWand = function() {
+    canTrigger = false;
+    var resetCanTrigger = function() {
+      canTrigger = true;
+    }
+    setTimeout(resetCanTrigger, 500);
+
+    loadAllSounds();
+  }
+
+  //============================================
+  // Handle All The Device Motion
 
   var deviceMotionHandler = function(motion) {
-    // Grab the acceleration from the results
-
     var acceleration = motion.acceleration;
 
     var magnitude = Math.sqrt(
@@ -40,33 +40,69 @@ $(document).ready(function() {
       Math.pow(acceleration.z, 2)
     )
 
-    console.log(magnitude)
+    console.log("Magnitude: " + magnitude);
 
     if (magnitude > THRESHOLD_MAGNITUDE && canTrigger == true) {
-      canTrigger = false;
-      setTimeout(function() {canTrigger = true;}, 500);
-
-      var randomSoundFileIndex = Math.floor((Math.random() * sounds.length - 1) + 1);
-      var sound = sounds[randomSoundFileIndex];
-      console.log(sound);
-
-      sound.play();
-
-      dweetio.dweet_for("10-points-for-gryffindor", {time: new Date(), type: "trigger"}, function(err, dweet){
-        // console.log(dweet.thing); // The generated name
-        // console.log(dweet.content); // The content of the dweet
-        // console.log(dweet.created); // The create date of the dweet
-      });
-
+      waveWand();
     }
   }
 
-  if (window.DeviceMotionEvent) {
-    window.addEventListener('devicemotion', deviceMotionHandler, false);
-  } 
-  else {
-    alert("Your device cannot cast spells on this wand. Try an iOS or Android wand...")
+  //============================================
+  // Load All Sounds
+
+  var loadAllSounds = function() {
+    var soundFilenames = [
+      "Expeliarmus 1.mp3",
+      "Stupefix + Contre 1.mp3",
+      "Expeliarmus 2.mp3",
+      "Transplanage Arrive.mp3",
+      "Stupefix + Contre 2.mp3"
+    ];
+
+    $(soundFilenames).each(function(i, filename) {
+      sounds[i] = new Howl({urls: ['sounds/' + filename] });
+    });
   }
 
-})
+  //============================================
+  // Play All Sounds
+
+  var playAllSounds = function() {
+    sounds.forEach(function(sound) {
+      sound.play();
+    });
+  }
+
+  //============================================
+  // Play All Sounds
+
+  var playRandomSound = function() {
+    var randomSoundFileIndex = Math.floor((Math.random() * sounds.length - 1) + 1);
+    var sound = sounds[randomSoundFileIndex];
+    sound.play();
+  }
+
+  var initialize = function() {
+    $("button").on("click touchstart", function(touchEvent) {
+      $("button").hide();
+      $("#volume").hide();
+      
+      $("#wave").show();
+
+      playAllSounds();
+    });
+
+    if (window.DeviceMotionEvent) {
+      window.addEventListener('devicemotion', deviceMotionHandler, false);
+    } 
+    else {
+      alert("You cannot cast spells on this wand. Try another iOS or Android wand...")
+    }
+  }
+
+  initialize();
+
+}
+
+$(document).ready(theFunctionToCallWhenTheDocumentIsReady);
 
